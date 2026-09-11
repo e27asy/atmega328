ef6dacafe4cb33516ad6697620f7ff8f95eac536
e27asy <e27asy@gmail.com>
2026-09-11 11:22:52 +0700

    oled+dht11

--- lib/DHT/examples/DHTtester/DHTtester.ino
+++ lib/DHT/examples/DHTtester/DHTtester.ino
@@ -1,54 +1,41 @@
-#include <Arduino.h>
-
-#include <Wire.h> 
-#include <SPI.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-Adafruit_SSD1306 OLED(-1); 
-
+// Example testing sketch for various DHT humidity/temperature sensors
+// Written by ladyada, public domain
 
 #include "DHT.h"
 
 #define DHTPIN 2     // what digital pin we're connected to
 
-#define DHTTYPE DHT11  // DHT 22  (AM2302), AM2321
+// Uncomment whatever type you're using!
+//#define DHTTYPE DHT11   // DHT 11
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
+// Connect pin 1 (on the left) of the sensor to +5V
+// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
+// to 3.3V instead of 5V!
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 4 (on the right) of the sensor to GROUND
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
+// Initialize DHT sensor.
+// Note that older versions of this library took an optional third parameter to
+// tweak the timings for faster processors.  This parameter is no longer needed
+// as the current DHT reading algorithm adjusts itself to work on faster procs.
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(9600);
-  OLED.begin(SSD1306_SWITCHCAPVCC,0x3C);
   Serial.println("DHTxx test!");
 
   dht.begin();
-
-
-
-  
-  // ล้างหน้าจอ
-  OLED.clearDisplay(); 
-// กำหนดสีตัวอักษร
-  OLED.setTextColor(WHITE,BLACK);   //Text is white ,background is black
-  OLED.setCursor(0, 0);
-  OLED.setTextSize(2); 
-  OLED.println("LBTECH");
-
-
-  // ตำแหน่ง
- // OLED.setCursor(5, 0);
- // OLED.print( "temperature" );
-
-  
-  // ส่งข้อมูลไป OLED
-  OLED.display();
-
 }
 
 void loop() {
   // Wait a few seconds between measurements.
   delay(2000);
 
-
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   float h = dht.readHumidity();
   // Read temperature as Celsius (the default)
   float t = dht.readTemperature();
@@ -79,27 +66,4 @@ void loop() {
   Serial.print(" *C ");
   Serial.print(hif);
   Serial.println(" *F");
- 
- 
-  OLED.clearDisplay();
-  // กำหนดสีตัวอักษร
-  OLED.setTextColor( SSD1306_WHITE );
-  // ขนาดตัวอักษร
-  OLED.setTextSize(2);
-  // ตำแหน่ง
-  OLED.setCursor(5, 0);
-  OLED.print( "T:" );
-  OLED.print(t);
-  OLED.print( " *C" );
-  OLED.setCursor(5, 20);
-  OLED.print( "H:" );
-  OLED.print(h);
-  OLED.println( " %" );
-  // ส่งข้อมูลไป OLED
-  OLED.display();
-
-
-
 }
-
-
